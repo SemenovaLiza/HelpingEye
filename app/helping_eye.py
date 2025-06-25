4a5d4db61eb359e34bae726e2ebd6ddac2ab22e0
Семенова Елизавета <thealeeeaaa23@gmail.com>
2025-06-24 20:49:38 -0400

    code v1

---
+++ app/helping_eye.py
@@ -0,0 +1,199 @@
+from datasets import load_dataset, Features, Sequence, ClassLabel, Value, Array2D, Array3D
+from datasets.features import ClassLabel
+from evaluate import load
+import numpy as np
+from PIL import ImageDraw, ImageFont
+import torch
+from transformers import AutoModelForTokenClassification, AutoProcessor, LayoutLMv3ForTokenClassification, TrainingArguments, Trainer
+from transformers.data.data_collator import default_data_collator
+
+processor = AutoProcessor.from_pretrained("microsoft/layoutlmv3-base", apply_ocr=False)
+dataset = load_dataset("nielsr/funsd-layoutlmv3") # layoutlmv ver 3 is a model for recognizing structured objects
+features = dataset["train"].features
+column_names = dataset["train"].column_names
+image_column_name = "image"
+text_column_name = "tokens"
+boxes_column_name = "bboxes"
+label_column_name = "ner_tags"
+
+training_args = TrainingArguments(output_dir="test",
+                                  max_steps=1000,
+                                  per_device_train_batch_size=2,
+                                  per_device_eval_batch_size=2,
+                                  learning_rate=1e-5,
+                                  eval_strategy="steps",
+                                  eval_steps=100,
+                                  load_best_model_at_end=True,
+                                  metric_for_best_model="f1")
+
+
+def get_label_list(labels):
+    """Creates a list of unique labels that will be used
+    to create mappings.
+    """
+    unique_labels = set()
+    for label in labels:
+        unique_labels |= set(label) # set union
+    label_list = list(unique_labels)
+    label_list.sort()
+    return label_list
+
+def create_mapping(list_, reverse=False):
+    """Creates mapping fron list."""
+    mapping = {k: v for k, v in enumerate(list_)}
+    return mapping
+
+# This data will be used to interpret the results.
+if isinstance(features[label_column_name].feature, ClassLabel):
+    print("YES IS INST", features[label_column_name].feature)
+    label_list = features[label_column_name].feature.names
+    id2label = create_mapping(label_list)
+    label2id = create_mapping(label_list, reverse=True)
+else:
+    print("NOOOOO", features[label_column_name].feature)
+    label_list = get_label_list(dataset["train"].label_column_name)
+    id2label = create_mapping(label_list)
+    label2id = create_mapping(label_list, reverse=True)
+num_labels = len(label_list)
+
+def prepare_examples(examples):
+    images = examples[image_column_name]
+    words = examples[text_column_name]
+    boxes = examples[boxes_column_name]
+    word_labels = examples[label_column_name]
+
+    encoding = processor(images, words, boxes=boxes, word_labels=word_labels,
+                         truncation=True, padding="max_length")
+    return encoding
+
+features = Features({
+    'pixel_values': Array3D(dtype='float32', shape=(3, 224, 224)),
+    'input_ids': Sequence(feature=Value(dtype='int64')),
+    'attention_mask': Sequence(Value(dtype='int64')),
+    'bbox': Array2D(dtype='int64', shape=(512, 4)),
+    'labels': Sequence(feature=Value(dtype='int64')),
+})
+
+def create_dataset(dataset_, features, column_names):
+    result = dataset[dataset_].map(
+        prepare_examples,
+        batched=True,
+        remove_columns=column_names,
+        features=features,
+    )
+    return result
+
+train_dataset = dataset['train'].map(
+    prepare_examples,
+    batched=True,
+    remove_columns=column_names,
+    features=features,
+)
+eval_dataset = dataset['test'].map(
+    prepare_examples,
+    batched=True,
+    remove_columns=column_names,
+    features=features,
+)
+example = train_dataset[0]
+processor.tokenizer.decode(example['input_ids'])
+train_dataset.set_format("torch")
+#print(processor.tokenizer.decode(example['input_ids']))
+example = train_dataset[0]
+for k, v in example.items():
+    print(k, v.shape)
+
+metric = load('seqeval')
+return_entity_level_metrics = False
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    results = metric.compute(predictions=true_predictions, reference=true_labels)
+    if return_entity_level_metrics:
+        final_results = {}
+        for key, value in results.items():
+            if isinstance(value, dict):
+                for n, v in value.items():
+                    final_results[f"{key}_{n}"] = v
+            else:
+                final_results[key] = value
+        return final_results
+    else:
+        return {
+            "precision": results["overall precision"],
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
+            "accuracy": results["overall_accuract"],
+        }
+    
+model = LayoutLMv3ForTokenClassification.from_pretrained("microsoft/layoutlmv3-base",
+                                                         id2label=id2label, label2id=label2id)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    tokenizer=processor,
+    data_collator=default_data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+# print(trainer.evaluate())
+model = AutoModelForTokenClassification.from_pretrained("/content/test/checkpoint-1000")
+example = dataset["test"][1]
+image = example["image"]
+words = example["tokens"]
+boxes = example["bboxes"]
+word_labels = example["ner_tags"]
+
+encoding = processor(image, words, boxes=boxes, word_labels=word_labels, return_tensors="pt")
+with torch.no_grad():
+  outputs = model(**encoding)
+
+logits = outputs.logits
+predictions = logits.argmax(-1).squeeze().tolist()
+labels = encoding.labels.squeeze().tolist()
+
+def unnormalize_box(bbox, width, height):
+     return [
+         width * (bbox[0] / 1000),
+         height * (bbox[1] / 1000),
+         width * (bbox[2] / 1000),
+         height * (bbox[3] / 1000),
+     ]
+
+token_boxes = encoding.bbox.squeeze().tolist()
+width, height = image.size
+
+true_predictions = [model.config.id2label[pred] for pred, label in zip(predictions, labels) if label != - 100]
+true_labels = [model.config.id2label[label] for prediction, label in zip(predictions, labels) if label != -100]
+true_boxes = [unnormalize_box(box, width, height) for box, label in zip(token_boxes, labels) if label != -100]
+
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.load_default()
+
+def iob_to_label(label):
+    label = label[2:]
+    if not label:
+      return 'other'
+    return label
+
+label2color = {'question':'blue', 'answer':'green', 'header':'orange', 'other':'violet'}
+
+for prediction, box in zip(true_predictions, true_boxes):
+    predicted_label = iob_to_label(prediction).lower()
+    draw.rectangle(box, outline=label2color[predicted_label])
+    draw.text((box[0] + 10, box[1] - 10), text=predicted_label, fill=label2color[predicted_label], font=font)
+print(image)
